69f88406502af937e7ed2e9b2491c35782f52ac6
Carlos Escobar <escobarcarlos@gmail.com>
2022-02-09 21:53:51 -0800

    add-css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,9 +6,13 @@ function Home() {
   return( 
   
   
-    <Wrap>
-        <h1>DEXjijn</h1>
-    </Wrap>
+    <Container>
+
+        <LogoText>
+        <h1>DEX</h1>
+        </LogoText>
+       
+    </Container>
     
     
     
@@ -17,12 +21,19 @@ function Home() {
     )
 }
 
-const Wrap = styled.div`
-    height:80vh;
-    h1{
-        color: red;
-    }
-`
+const Container = styled.div`
+    height:100vh;
+    width:100vw;
 
+
+`
+const LogoText = styled.div`
+    text-align: center;
+    margin-top: 12vh;
+    font-size: 20px;
+    font-style: italic;
+    color:purple;
+
+`
 
 export default Home;
